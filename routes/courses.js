dc4a071c16a0868bb9284fa2a368617c98b77d8c
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-03-31 16:22:15 +0530

    user and courses routes

---
+++ routes/courses.js
@@ -0,0 +1,21 @@
+const {Router}= require("express")
+const coursesRouter=Router();
+
+// route to see all available courses
+coursesRouter.get("/preview",function(req,res){
+
+    res.json({
+        message:"courses end point"
+    })
+})
+// route to purchase a course
+coursesRouter.post("/purchase",function(req,res){
+
+    res.json({
+        message:"end point"
+    })
+})
+
+module.exports={
+    coursesRouter:coursesRouter
+}
