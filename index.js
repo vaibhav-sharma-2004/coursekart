192ab022131c8480f4838ae66ce7f044ab319e62
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-03-31 16:20:05 +0530

    added routes

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
 const express=require("express");
+const {userRouter}=require("./routes/user")
+const {coursesRouter}= require("./routes/courses")
 const app=express();
 
-// route for user signup
-app.post("/user/signup",function(req,res){
+app.use("/user",userRouter)
+app.use("/courses",coursesRouter)
 
-    res.json({
-        message:"signup end point"
-    })
-})
-
-// route for user sign in
-app.post("/user/signin",function(req,res){
-
-    res.json({
-        message:"signin end point"
-    })
-})
-
-// route to see all available courses
-app.get("/courses",function(req,res){
-
-    res.json({
-        message:"courses end point"
-    })
-})
-
-// route to see all purchased courses by user
-app.get("/user/purchases",function(req,res){
-
-    res.json({
-        message:"purchase end point"
-    })
-})
-
-// route to purchase a course
-app.post("user/courses/purchase",function(req,res){
-
-    res.json({
-        message:"end point"
-    })
-})
+app.listen(3000);
