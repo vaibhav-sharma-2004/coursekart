3fd3afe7adab66ec2a292ac75c0639a2c72539d3
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-04-05 22:26:55 +0530

    add database

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express=require("express");
+const mongoose=require("mongoose");
+
 const {userRouter}=require("./routes/user")
 const {coursesRouter}= require("./routes/courses")
+const {adminRouter}=require("./routes/admin")
 const app=express();
+
+app.use(express.json())
 
 app.use("/user",userRouter)
 app.use("/courses",coursesRouter)
+app.use("/admin",adminRouter)
 
-app.listen(3000);
+async function main() {
+    await mongoose.connect("mongodb+srv://admin:94UeW2cQ6OnRQKrK@cluster0.pftveuj.mongodb.net/coursekart")
+    app.listen(3000)
+
+}
+
+main()
