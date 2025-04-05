8a1e830c33bc45047898947b57d25dc5a8505a05
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-04-05 22:25:37 +0530

    user route structure

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,21 @@
 const {Router}=require("express")
+const userModel=require("../db")
 const userRouter=Router();
+const jwt=require("jsonwebtoken")
+const JWT_USER_PASSWORD="1212121"
 
 // route for user signup
-userRouter.post("/signup",function(req,res){
-
+userRouter.post("/signup",async function(req,res){
+    
+    const { email, password, firstName, lastName}=req.body
+        
+        await adminModel.create({
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        })
+    
     res.json({
         message:"signup end point"
     })
@@ -12,9 +24,31 @@ userRouter.post("/signup",function(req,res){
 // route for user sign in
 userRouter.post("/signin",function(req,res){
 
-    res.json({
-        message:"signin end point"
+    const { email,password }= req.body
+
+    const user= userModel.findOne({
+        email:email,
+        password:password
     })
+
+    if(user){
+
+        const token=jwt.sign({
+            id: user._id
+        },JWT_USER_PASSWORD);
+
+        res.json({
+            token:token
+        })
+    }
+
+
+
+    else{
+        res.status(403).json({
+            message:"Incorrect credentials"
+        })
+    }
 })
 
 
