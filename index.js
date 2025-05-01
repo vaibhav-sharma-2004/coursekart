fac24692f6ea5bc5dc27a41a63ef482de8a06f71
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-05-01 15:28:38 +0530

    imported env

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-requiwe("dotenv").config()
+require("dotenv").config()
 const express=require("express");
 const mongoose=require("mongoose");
 
@@ -14,7 +14,7 @@ app.use("/courses",coursesRouter)
 app.use("/admin",adminRouter)
 
 async function main() {
-    await mongoose.connect("process.env.MONGO_URL")
+    await mongoose.connect(process.env.MONGO_URL)
     app.listen(3000)
 
 }
