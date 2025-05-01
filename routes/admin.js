2c22aea1f23e47fee4f2b688a75c88073ddbd911
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-05-01 15:23:12 +0530

    admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,40 +1,89 @@
 const {Router}=require("express")
+const {adminModel}=require("../db");
 const adminRouter=Router();
+const jwt =require("jsonwebtoken");
+const { adminMiddleware } = require("../middleware/admin");
+const JWT_ADMIN_PASSWORD="ADMIN1212"
 
 // route for admin signup
-adminRouter.post("/signup",function(req,res){
+adminRouter.post("/signup",async function(req,res){
+
+    const {email,password, firstName, lastName}=req.body
+    
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
 
     res.json({
-        message:"signup end point"
+        message:"signup success"
     })
 })
 
 // route for admin sign in
-adminRouter.post("/signin",function(req,res){
+adminRouter.post("/signin",async function(req,res){
 
-    res.json({
-        message:"signin end point"
-    })
+    const { email,password }= req.body;
+    
+    //
+    const admin= await adminModel.findOne({
+        email:email,
+        password:password
+    });
+
+    if(admin){
+
+        const token=jwt.sign({
+            id: admin._id
+        },JWT_ADMIN_PASSWORD);
+
+        //do cookie logic if using cookie instead of token
+        res.json({
+            token:token
+        })
+    }
+
+    else{
+        res.status(403).json({
+            message:"Incorrect credentials"
+        })
+    }
 })
 
-// route for admin to post course
-adminRouter.post("/courses",function(req,res){
-
+// route for admin to update course
+adminRouter.put("/courses",function(req,res){
+    
     res.json({
         message:"preview end point"
     })
 })
 
-// route for admin to update course
-adminRouter.put("/signin",function(req,res){
+// route for admin to post course
+adminRouter.post("/courses",adminMiddleware,async function(req,res){
+    
+    const adminId=req.adminId;
+
+    const {title,description,price,imageUrl}=req.body
+
+    const course=await courseModel.create({
+        title:title,
+        description:description,
+        price:price,
+        imageUrl:imageUrl,
+        creator:adminId
+    })
 
     res.json({
-        message:"update end point"
+        message:"course created successfully",
+        courseId:course._id
+
     })
 })
 
 // route for admin to see courses
-adminRouter.get("/courses",function(req,res){
+adminRouter.get("/courses/bulk",function(req,res){
 
     res.json({
         message:"signin end point"
