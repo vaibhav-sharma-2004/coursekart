7ac251adbabff8296fc8c6e5136a4d3857bc4874
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-05-01 15:24:25 +0530

    courses routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,16 +1,29 @@
 const {Router}= require("express")
+const {userMiddleware}=require("../middleware/user");
+const { purchaseModel,courseModel } = require("../db");
 const coursesRouter=Router();
 
 // route to see all available courses
-coursesRouter.get("/preview",function(req,res){
+coursesRouter.get("/preview",async function(req,res){
+
+    const courses= await courseModel.find({});
+    console.log(courses)
 
     res.json({
-        message:"courses end point"
+        message:"preveiw end point"
     })
 })
 // route to purchase a course
-coursesRouter.post("/purchase",function(req,res){
+coursesRouter.post("/purchase",userMiddleware,async function(req,res){
 
+    const {courseId}=req.body;
+    const userId=req.user.id;
+    console.log(courseId,userId)
+
+    await purchaseModel.create({
+        courseId:courseId,
+        userId:userId
+    })
     res.json({
         message:"course purchase end point"
     })
